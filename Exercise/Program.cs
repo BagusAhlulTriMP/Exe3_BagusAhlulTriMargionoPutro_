2b5e0730da8e2291ee596f77424798562de749d0
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2022-11-23 16:19:14 +0700

    add list traverse all nodes

--- Exercise/Program.cs
+++ Exercise/Program.cs
@@ -48,9 +48,17 @@ namespace Exercise_Linked_List_A
                 Console.WriteLine("\nList is empty");
             else
             {
-
+                Console.WriteLine("\nRecords in the list are:\n");
+                Node currentNode;
+                currentNode = LAST.next;
+                while (currentNode != LAST)
+                {
+                    Console.Write(currentNode.rollNumber + "   " + currentNode.name + "\n");
+                    currentNode = currentNode.next;
+                }
+                Console.Write(LAST.rollNumber + "   " + LAST.name + "\n");
             }
         }
     }
     }
-}
+
